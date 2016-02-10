d0cfd025e0b34e3a06082c03faeb8c89d3a607d1
Raejoon Jung <raejoon@stanford.edu>
2016-02-10 11:01:29 -0800

    basic file transfer with udp (no libraptorq)

--- src/receiver.cc
+++ src/receiver.cc
@@ -1,51 +1,62 @@
 #include <iostream>
 #include <cstdlib>
+#include <fstream>
+#include <string>
 
 #include "socket.hh"
 
-UDPSocket listen_wrapper() {
+UDPSocket listen_wrapper(Address& senderAddr) {
     UDPSocket sock = UDPSocket();
     sock.bind(Address("0", "0"));
     Address localaddr = sock.local_address();
     std::cerr << localaddr.ip() << " " << localaddr.port() << std::endl;
 
-    std::cout << sock.recv().payload;
+    UDPSocket::received_datagram dg = sock.recv();
+
+    std::cout << dg.payload;
+
+    senderAddr = dg.source_address;
 
     return sock;
 }
 
-int main(int argc, char* argv[]) {
-    // check the command-line arguments
-    if (argc < 1) abort();
-    if (argc != 1) {
-        std::cerr << "Usage: " << argv[0] << endl;
+size_t respondHandshake(UDPSocket& sk, Address& senderAddr) {
+    size_t filesz = stoi(sk.recv().payload);     
+    const char data[2] = {(char)0xf2, '\0'};
+    const std::string payload = data;
+    sk.sendto(senderAddr, payload);
+    std::cout << "Ready to receive file of size: " << filesz << std::endl;
+    return filesz;
+}
+
+int main( int argc, char *argv[] )
+{
+    /* check the command-line arguments */
+    if ( argc < 1 ) { abort(); } /* for sticklers */
+    if ( argc != 1 ) {
+        cerr << "Usage: " << argv[ 0 ] << endl;
         return EXIT_FAILURE;
     }
 
-    // listen connection
-    UDPSocket sk = listen_wrapper();
-    /*
-    // block until next request; send back ACK
-    int result = processHandshake(memory_size, common_metadata, scheme_specific_metadata);   // might need total num of blocks
-    if (result == -1) {...}
+    Address senderAddr;
+    UDPSocket sk = listen_wrapper(senderAddr);
+    size_t filesz = respondHandshake(sk, senderAddr);
 
-    // open receive file
-    ofstream ofile = openFile(std::string filename);
+    std::ofstream outfile;
+    outfile.open("recvfile", ios::out | ios::binary);
 
-    int numBlocks = 0;
-    while (true) {
-        // double-check the metadata can be reused for different instances
-        RaptorQ::Decoder decoder(common_metadata, scheme_specific_metadata);
+    size_t recvsz = 0;
+    while(recvsz < filesz) {
+        std::string payload = sk.recv().payload;
+        size_t bufsz = payload.length();
+        const char* buffer = payload.c_str();
 
-        std::vector<Alignment> block;
-        while (!decoder.decode(block.begin(), block.end())) {
-            std::vector<Alignment> symbol = receiveSymbol(sk);
-            decoder.add_symbol(symbol.begin(), symbol.end());
-        }
-        sendACK();
-        size_t file_size = appendToFile(ofile, block);
-        if (++numBlocks == totalNumOfBlocks) break;
+        recvsz += bufsz; 
+        if (recvsz > filesz) outfile.write(buffer, bufsz - (recvsz - filesz));
+        else outfile.write(buffer, bufsz);
     }
-    */
+
+    cout << "Got entire file" << endl;
+    outfile.close();
     return EXIT_SUCCESS;
 }
