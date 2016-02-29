db8af40e3352a0fee40554ec769b8d185bd07eae
Raejoon Jung <raejoon@stanford.edu>
2016-02-29 13:18:02 -0800

    progress bar added to sender.cc

--- src/sender.cc
+++ src/sender.cc
@@ -1,11 +1,72 @@
 #include <iostream>
 #include <fstream>
 #include <RaptorQ.hpp>
+#include <chrono>
+#include <iomanip>
 
 #include "tub.hh"
 #include "common.hh"
 #include "socket.hh"
 #include "wire_format.hh"
+
+struct progress_t {
+  std::chrono::time_point<std::chrono::system_clock> start;
+  std::chrono::time_point<std::chrono::system_clock> current;
+  double elapsed_seconds;
+  uint64_t filesize;
+  uint64_t sentsize;
+};
+
+void initialize_progress(progress_t& progress, uint64_t filesize) {
+  progress.filesize = filesize;
+  progress.start = std::chrono::system_clock::now();
+}
+
+void update_progress(progress_t& progress, uint64_t sentsize) {
+  progress.sentsize = sentsize;
+  progress.current = std::chrono::system_clock::now();
+  std::chrono::duration<double> diff = progress.current - progress.start;
+  progress.elapsed_seconds = diff.count();
+}
+
+void update_progressbar(progress_t& progress) {
+  float fraction = progress.sentsize/((float) progress.filesize);
+  int barwidth = 70;
+  std::cout <<"[";
+  int pos = barwidth * fraction;
+  for (int i = 0; i < barwidth; ++i) {
+    if (i < pos) std::cout << "=";
+    else if (i == pos) std::cout << ">";
+    else std::cout << " ";
+  }
+  std::cout << "] " << int(fraction * 100.0) << " % ";
+
+  int hrs = int(progress.elapsed_seconds) / 3600;
+  int mins = (int(progress.elapsed_seconds) % 3600) / 60;
+  int secs = (int(progress.elapsed_seconds)) % 60;
+  std::cout << std::setw(2) << std::setfill('0') << hrs << ":";
+  std::cout << std::setw(2) << std::setfill('0') << mins << ":";
+  std::cout << std::setw(2) << std::setfill('0') << secs << " ";
+
+  if (progress.elapsed_seconds > 1.0) {
+    double rate = progress.sentsize / progress.elapsed_seconds;
+    int leftsize = progress.filesize - progress.sentsize;
+    double remaining_seconds = leftsize / rate;
+
+    hrs = int(remaining_seconds) / 3600;
+    mins = (int(remaining_seconds) % 3600) / 60;
+    secs = (int(remaining_seconds)) % 60;
+    std::cout << "ETA: ";
+    std::cout << std::setw(2) << std::setfill('0') << hrs << ":";
+    std::cout << std::setw(2) << std::setfill('0') << mins << ":";
+    std::cout << std::setw(2) << std::setfill('0') << secs;
+  } 
+  else std::cout << "ETA: --:--:--";
+
+  std::cout << "\r";
+  std::cout.flush();
+  if (fraction >= 1.0) std::cout << std::endl;
+}
 
 /**
  * Starts the handshake procedure with the receiver. This method needs to
@@ -93,6 +154,10 @@ void sendSymbol(UDPSocket *udpSocket,
 void transmit(RaptorQEncoder& encoder,
               UDPSocket* udpSocket)
 {
+    progress_t progress;
+    int sentsize = 0;
+    initialize_progress(progress, encoder.blocks());
+
     std::vector<RaptorQSymbolIterator> repairSymbolIters;
     for (const auto& block : encoder) {
         repairSymbolIters.push_back(block.begin_repair());
@@ -105,6 +170,9 @@ void transmit(RaptorQEncoder& encoder,
     const static uint32_t REPAIR_SYMBOL_SEND_INTERVAL = MAX_SYM_PER_BLOCK / 10;
     uint32_t numSourceSymbolSent = 0;
     UDPSocket::received_datagram datagram {Address(), 0, 0, 0};
+
+    update_progress(progress, sentsize);
+    update_progressbar(progress);
     for (uint8_t currBlock = 0; currBlock < encoder.blocks(); currBlock++) {
         const auto &block = *encoder.begin().operator++(currBlock);
         RaptorQSymbolIterator sourceSymbolIter = block.begin_source();
@@ -128,6 +196,9 @@ void transmit(RaptorQEncoder& encoder,
                 }
             }
         }
+        sentsize += 1;
+        update_progress(progress, sentsize);
+        update_progressbar(progress);
     }
 
     while (decodedBlocks.count() < encoder.blocks()) {
